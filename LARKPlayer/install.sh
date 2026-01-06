291a43f10ecbde7dda95821413389f0e9a9d6aa6
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2026-01-06 16:24:28 +0000

    Stuff

--- LARKPlayer/install.sh
+++ LARKPlayer/install.sh
@@ -7,15 +7,12 @@ ZIPNAME=lark.zip
 
 mkdir -p "$TMPDIR"
 
-# Download latest release
 curl -fSL --progress-bar \
   -o "$TMPDIR/$ZIPNAME" \
   https://github.com/kbarni/LARKPlayer/releases/latest/download/lark.zip
 
-# Extract directly to /mnt/us
 unzip -oq "$TMPDIR/$ZIPNAME" -d /mnt/us/
 
-# Cleanup (normal durumda)
 rm -rf "$TMPDIR"
 
 exit 0
--- LARKPlayer/uninstall.sh
+++ LARKPlayer/uninstall.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-# documents
 rm -f /mnt/us/documents/lark.sh
-
-# extensions
 rm -rf /mnt/us/extensions/lark
-
-# main binary + libs
 rm -rf /mnt/us/LARK
 
 exit 0
